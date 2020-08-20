05bb94d16edd0699e9b1dc86441a5f080e0b0591
Dave Trimboli <davidatrimboli@gmail.com>
2020-08-20 12:46:14 -0400

    added title line 22, brought in uuid

--- routes/itemRoutes.js
+++ routes/itemRoutes.js
@@ -18,42 +18,23 @@ router.post('/notes', (req, res) => {
     if (err) { console.log(err) }
 
     let notes = JSON.parse(data)
-    let item = {
+    let note = {
+      title: req.body.title,
       id: uuid.v1(),
       text: req.body.text,
-      isDone: req.body.isDone
+      
     }
-    notes.push(item)
+    notes.push(note)
 
     fs.writeFile(join(__dirname, '..', 'db', 'db.json'), JSON.stringify(notes), err => {
-      if (err) { console.log(err) }
+      if (err) { console.log(err) } 
 
-      res.json(item)
+      res.json(note)
     })
   })
 })
 
-// PUT one item
-router.put('/notes/:id', (req, res) => {
 
-  fs.readFile(join(__dirname, '..', 'db', 'db.json'), 'utf8', (err, data) => {
-    if (err) { console.log(err) }
-
-    let notes = JSON.parse(data)
-
-    for (let i = 0; i < notes.length; i++) {
-      if (notes[i].id === req.params.id) {
-        notes[i].isDone = req.body.isDone
-      }
-    }
-
-    fs.writeFile(join(__dirname, '..', 'db', 'db.json'), JSON.stringify(notes), err => {
-      if (err) { console.log(err) }
-
-      res.sendStatus(200)
-    })
-  })
-})
 
 // DELETE one item
 router.delete('/notes/:id', (req, res) => {
